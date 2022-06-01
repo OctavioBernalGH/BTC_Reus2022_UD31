900e9e1d1d5963b3a61ddb87934579d1406150e7
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-06-01 15:50:04 +0200

    Ejercicio 3

---
+++ Ejercicio_3/js/index.js
@@ -0,0 +1,25 @@
+/** SE IMPORTA EL JAVASCRIPT */
+/** Se pulsa el primer párrafo. */
+function externo() {
+    alert('Pulsado parrafo externo');
+}
+
+/** Se pulsa la tabla 1 la fila 1 y la columna 1 */
+function tabla11() {
+    alert('Se ha presionado "ROW1 COL1"');
+}
+
+/** Se pulsa la tabla 1 la fila 1 y la columna 2 */
+function tabla12(){
+    alert('Se ha presionado "ROW1 COL2"')
+}
+
+/** Se pulsa la tabla 2 la fila 1 y la columna 1 */
+function tabla21(){
+    alert('Se ha presionado "ROW2 COL1"')
+}
+
+/** Se pulsa la tabla 2 la fila 1 y la columna 2 */
+function tabla22(){
+    alert('Se ha presionado "ROW2 COL2"')
+}
