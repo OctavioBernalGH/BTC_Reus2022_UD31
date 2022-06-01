9cb891185115ebd7097efc61b468dcdf6ce16048
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-06-01 18:29:25 +0200

    Ejercicio 5

---
+++ Ejercicio_5/js/index.js
@@ -0,0 +1,39 @@
+function imagen1() {
+    document.getElementById("imgGrande").src = "img/foto1.jpg";
+    document.getElementById("piePagina").innerHTML = "Foto 1";
+}
+
+function imagen2() {
+    document.getElementById("imgGrande").src = "img/foto2.jpg";
+    document.getElementById("piePagina").innerHTML = "Foto 2";
+}
+
+function imagen3() {
+    document.getElementById("imgGrande").src = "img/foto3.jpg";
+    document.getElementById("piePagina").innerHTML = "Foto 3";
+}
+
+function imagen4() {
+    document.getElementById("imgGrande").src = "img/foto4.jpg";
+    document.getElementById("piePagina").innerHTML = "Foto 4";
+}
+
+function imagen5() {
+    document.getElementById("imgGrande").src = "img/foto5.jpg";
+    document.getElementById("piePagina").innerHTML = "Foto 5";
+}
+
+function imagen6() {
+    document.getElementById("imgGrande").src = "img/foto6.jpg";
+    document.getElementById("piePagina").innerHTML = "Foto 6";
+}
+
+function imagen7() {
+    document.getElementById("imgGrande").src = "img/foto7.jpg";
+    document.getElementById("piePagina").innerHTML = "Foto 7";
+}
+
+function imagen8() {
+    document.getElementById("imgGrande").src = "img/foto8.jpg";
+    document.getElementById("piePagina").innerHTML = "Foto 8";
+}
